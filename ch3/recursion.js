a5ea47adfe602a64c720d603085c30d575400696
Jared Parker <jared.parker@gmail.com>
2018-03-21 17:42:20 -0500

    recursion

--- ch3/recursion.js
+++ ch3/recursion.js
@@ -2,10 +2,12 @@ function isEven(num) {
   if(num === 0) return true;
   if(num === 1) return false;
 
-  return isEven(num - 2);
+  return (num < 0) ? isEven(num + 2) : isEven(num - 2);
 }
 
 console.log(isEven(0));
 console.log(isEven(1));
 console.log(isEven(100));
 console.log(isEven(101));
+
+console.log(isEven(-1));
