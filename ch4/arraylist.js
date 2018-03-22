268b46f807eb5c10829cad4a9fecc774b863bfdb
Jared Parker <jared.parker@gmail.com>
2018-03-21 20:40:02 -0500

    nth

--- ch4/arraylist.js
+++ ch4/arraylist.js
@@ -1,18 +1,27 @@
 function arrayToList(array, list = {}){
   if(array.length === 0) return null;
-  list.value = array.pop();
+  list.value = array.shift();
   list.rest = arrayToList(array);
   return list;
 }
 
 function listToArray(list, array = []) {
   if (list === null) return array;
-  array.unshift(list.value);
+  array.push(list.value);
   return listToArray(list.rest, array);
 }
 
 function prepend(list, val){
-  return arrayToList([val], list);
+  return {
+    value: val,
+    rest: list
+  }
+}
+
+function nth(list, depth){
+  if (depth === 0) return list.value;
+  if (!list.rest) return undefined;
+  return nth(list.rest, --depth);
 }
 
 let list = arrayToList([1,2,3])
@@ -23,3 +32,6 @@ let array = listToArray(list);
 
 console.log(array);
 
+// console.log(prepend(list, 9));
+
+console.log(nth(list, 2));
